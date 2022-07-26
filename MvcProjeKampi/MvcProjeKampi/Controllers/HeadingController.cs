bbecb66889d55af51c6e8ccfd95c2db5063b3b9d
Recep Batur <recep_btr44@hotmail.com>
2022-07-27 00:06:22 +0300

    Başlık crud işlemleri gerçekleştirildi.
    Başlık crud işlemleri gerçekleştirildi.

---
+++ MvcProjeKampi/MvcProjeKampi/Controllers/HeadingController.cs
@@ -0,0 +1,62 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameWork;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class HeadingController : Controller
+    {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var headingValues = hm.GetList();
+            return View(headingValues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            //Başlığa ait kategorileri dropdown list'e listeleme işlemi yaptık.
+            List<SelectListItem> valueCategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                    Text=x.CategoryName,
+                                                    Value=x.CategoryID.ToString()
+                                                  }
+                                                  ).ToList();
+
+            //Yeni başlık eklerken başlığı ekleyecek yazarları dropdownlist'e listeledik.
+            List<SelectListItem> valueWriter = (from x in wm.GetList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName + " " + x.WriterSurname,
+                                                    Value = x.WriterID.ToString()
+                                                }
+                                                ).ToList();
+
+            ViewBag.vlc = valueCategory;
+            ViewBag.vlw = valueWriter;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.HeadingAdd(p);
+            //HeadingValidator headingValidator = new HeadingValidator();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ContentByHeading()
+        {
+
+            return View();
+        }
+    }
+}
