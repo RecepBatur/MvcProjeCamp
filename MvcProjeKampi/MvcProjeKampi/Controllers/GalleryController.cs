c0bc2f15150c06bdd6a6ab01a7f9bfcd93093996
Recep Batur <recep_btr44@hotmail.com>
2022-08-05 01:20:33 +0300

    Galeri sayfası oluşturuldu. Resimler eklendi.

--- MvcProjeKampi/MvcProjeKampi/Controllers/GalleryController.cs
+++ MvcProjeKampi/MvcProjeKampi/Controllers/GalleryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,13 @@ namespace MvcProjeKampi.Controllers
     public class GalleryController : Controller
     {
         // GET: Gallery
+
+        ImageFileManager ifm = new ImageFileManager(new EfImageFileDal());
+
         public ActionResult Index()
         {
-            return View();
+            var files = ifm.GetList();
+            return View(files);
         }
     }
 }
