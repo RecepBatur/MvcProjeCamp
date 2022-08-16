3ef9f4538262cf74d4414f973b6288d23ec64a21
Recep Batur <recep_btr44@hotmail.com>
2022-08-16 22:46:23 +0300

    Yazar layout paneli entegre edildi.

---
+++ MvcProjeKampi/MvcProjeKampi/Controllers/WriterPanelController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class WriterPanelController : Controller
+    {
+        // GET: WriterPanel
+
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+
+        public ActionResult WriterProfile()
+        {
+            return View();
+        }
+        public ActionResult MyHeading(int id)
+        {
+            id = 4;
+            var myHeadingValues = hm.GetListByWriter(id);
+            return View(myHeadingValues);
+        }
+    }
+}
