2fdc772f5e65870e9231862fc204dea94e2f86ee
Recep Batur <recep_btr44@hotmail.com>
2022-08-16 23:53:18 +0300

    Yazar panelinde başlık crud işlemleri gerçekleştirildi.

--- MvcProjeKampi/MvcProjeKampi/Controllers/WriterPanelController.cs
+++ MvcProjeKampi/MvcProjeKampi/Controllers/WriterPanelController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFrameWork;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,16 +14,59 @@ namespace MvcProjeKampi.Controllers
         // GET: WriterPanel
 
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
-
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public ActionResult WriterProfile()
         {
             return View();
         }
-        public ActionResult MyHeading(int id)
+        public ActionResult MyHeading()
         {
-            id = 4;
-            var myHeadingValues = hm.GetListByWriter(id);
+            //id = 4;
+            var myHeadingValues = hm.GetListByWriter();
             return View(myHeadingValues);
+        }
+        [HttpGet]
+        public ActionResult NewHeading()
+        {
+            List<SelectListItem> valueCategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }
+                                                  ).ToList();
+
+            ViewBag.vlc = valueCategory;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.WriterID = 4;
+            p.HeadingStatus = true;
+            hm.HeadingAdd(p);
+            return RedirectToAction("MyHeading");
+        }
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valueCategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }
+                                                  ).ToList();
+            ViewBag.vlc = valueCategory;
+            var HeadingValue = hm.GetByID(id);
+            return View(HeadingValue);
+        }
+        [HttpPost]
+        public ActionResult EditHeading(Heading k)
+        {
+            hm.HeadingUpdate(k);
+            return RedirectToAction("MyHeading");
         }
     }
 }
