18c96ad4032855d51c0a45ebedc49002d526d648
Recep Batur <recep_btr44@hotmail.com>
2022-08-17 17:20:23 +0300

    Yazar mesaj sayfasında gelen ve gönderilen mesaj işlemleri entegre edildi.

---
+++ MvcProjeKampi/MvcProjeKampi/Controllers/WriterPanelMessageController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFrameWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class WriterPanelMessageController : Controller
+    {
+        // GET: WriterPanelMessage
+
+        MessageManager mm = new MessageManager(new EfMessageDal());
+        MessageValidator messageValidator = new MessageValidator();
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult Inbox()
+        {
+            var messageList = mm.GetListInbox();
+            return View(messageList);
+        }
+        public ActionResult SendBox()
+        {
+            var messageList = mm.GetListSendbox();
+            return View(messageList);
+        }
+        public PartialViewResult MessageListMenu()
+        {
+            return PartialView();
+        }
+    }
+}
