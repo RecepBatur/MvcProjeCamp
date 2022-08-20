fb41940eb93828fc2f5a8bc3f8044b58f6cd8f4a
Recep Batur <recep_btr44@hotmail.com>
2022-08-20 13:59:22 +0300

    Yazara göre login işlemi yapıldığında yazmış olduğu yazıları session ile taşıma işlemi yapıldı.

--- MvcProjeKampi/MvcProjeKampi/Controllers/WriterPanelContentController.cs
+++ MvcProjeKampi/MvcProjeKampi/Controllers/WriterPanelContentController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFrameWork;
 using System;
 using System.Collections.Generic;
@@ -13,11 +14,13 @@ namespace MvcProjeKampi.Controllers
         // GET: WriterPanelContent
         ContentManager cm = new ContentManager(new EfContentDal());
 
-        public ActionResult MyContent()
+        public ActionResult MyContent(string p)
         {
-            int id;
-            id = 2;
-            var contentValues = cm.GetListByWriter(id);
+            Context c = new Context();
+            p = (string)Session["WriterMail"];
+            var writerİdİnfo = c.Writers.Where(x => x.WriterMail == p).Select(y => y.WriterID).FirstOrDefault();
+            //ViewBag.d = p;
+            var contentValues = cm.GetListByWriter(writerİdİnfo);
             return View(contentValues);
 
         }
